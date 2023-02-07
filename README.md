e60e7c9c7e366c38fdfaa6e9f3c20dabe1263981
lilbitsnpieces <alreadywrote@gmail.com>
2023-02-06 19:39:05 -0800

    Add test 3

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Expected Output: 1
 Test: "It recognizes multiple vowels in a single word."
 Code: vowelCounter("cater");
 Expected Output: 2
+
+Test: "It recognizes a single vowel regardless of case."
+Code: vowelCounter("A");
+Expected Output: 1
--- js/scripts.js
+++ js/scripts.js
@@ -19,6 +19,18 @@ function vowelCounter(text) {
 }
 console.log(vowelCounter("cater"));
 
+function vowelCounter(text) {
+  const vowelList = "aeiouAEIOU";
+  let vowelCount = 0;
+  for (let i = 0; i < text.length; i++) {
+    if (vowelList.indexOf(text[i]) !== -1)
+    {
+      vowelCount += 1;
+    }
+  }
+  return vowelCount;
+}
+console.log(vowelCounter("AEIOUaeiou"));
 
 //UI Logic 
 
